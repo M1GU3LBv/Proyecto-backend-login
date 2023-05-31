73a40cba03d12afd07c9e193265a31193c5df17a
Noodlexz <60412364+M1GU3LBv@users.noreply.github.com>
2023-05-30 23:04:03 -0500

    a

---
+++ src/main/java/com/integradorbackendfr/IntegradorBackendfrApplication.java
@@ -0,0 +1,13 @@
+package com.integradorbackendfr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegradorBackendfrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntegradorBackendfrApplication.class, args);
+    }
+
+}
